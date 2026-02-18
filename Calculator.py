7e00539b76ba81a9197756c86c0a9d2d0255521c
cyberahmedshah <cyberahmedshah@gmail.com>
2026-02-19 04:11:27 +0500

    Added calculator

---
+++ Calculator.py
@@ -0,0 +1,10 @@
+y=75
+z=4
+a=y+z
+print("The value of ", y, "+", z, "=", a)
+b=y-z
+print("The value of ", y, "-", z, "=", b)
+c=y*z
+print("The value of ", y, "*", z, "=", c)
+d=y/z
+print("The value of ", y, "/", z, "=", d)
