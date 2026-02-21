0f2062ddd4b23af9e676959497940a32b7f050dc
cyberahmedshah <cyberahmedshah@gmail.com>
2026-02-22 02:10:54 +0500

    worked for efficient calculator

--- Calculator.py
+++ Calculator.py
@@ -8,3 +8,8 @@ c=y*z
 print("The value of ", y, "*", z, "=", c)
 d=y/z
 print("The value of ", y, "/", z, "=", d)
+x=int(input("Enter your first number: "))
+y=int(input("Enter your second number: "))
+z=int(input("Enter your third number: "))
+w=int(input("Enter your fourth number: "))
+print (x+y+z+w)
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 print("Hello, Automation World")
-print("This is my Python", "Automation", "Lab", sep="-" )
-dict1 = {"Name": "Ahmed", "Age": 19, "Driving Eligiblity": True }
-print(dict1)
+print("This is my Python", "Automation", "Lab", sep="-")
+dict1 = {"Name": "Ahmed", "Age": 19, "Driving Eligiblity": True}
+for key, value in dict1.items():
+    print(key, ":", value)
