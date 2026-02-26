d1859454c619c066ab8901eef22193c8b7aa5983
ahmedshah <cyberahmedshah@gmail.com>
2026-02-27 02:16:36 +0500

    improved greeting program

--- Greeting program.py
+++ Greeting program.py
@@ -3,8 +3,8 @@ Current_time = time.localtime()
 Current_hour =Current_time.tm_hour
 if 5 <= Current_hour <12:
     print("Good Morning")
-elif Current_hour <16:
+elif 12 <= Current_hour <16:
     print("Good Afternoon")
-elif Current_hour <21:
+elif 16 <= Current_hour <21:
     print("Good Evening")
 else:    print("Good Night")
