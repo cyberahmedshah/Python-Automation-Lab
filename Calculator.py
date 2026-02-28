29dd40b84f556d55943e370cad3462bdb5b34197
ahmedshah <cyberahmedshah@gmail.com>
2026-02-28 23:12:58 +0500

    added oprands for calculator

--- Calculator.py
+++ Calculator.py
@@ -1,15 +1,16 @@
-y=75
-z=4
-a=y+z
-print("The value of ", y, "+", z, "=", a)
-b=y-z
-print("The value of ", y, "-", z, "=", b)
-c=y*z
-print("The value of ", y, "*", z, "=", c)
-d=y/z
-print("The value of ", y, "/", z, "=", d)
+o=(input("Enter the operation: "))
+oprand=str(o)
 x=int(input("Enter your first number: "))
 y=int(input("Enter your second number: "))
 z=int(input("Enter your third number: "))
 w=int(input("Enter your fourth number: "))
-print (x+y+z+w)
+if oprand== "+":
+ print (x+y+z+w)
+elif oprand== "-":
+ print(x-y-z-w)
+elif oprand== "*":
+ print(x*y*z*w)
+elif oprand== "/":
+ print(x/y/z/w)
+else:
+ print("Invalid Operand")
