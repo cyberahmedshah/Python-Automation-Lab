ad1bf829b93343b715e731c2d4bc9e973db7c57e
ahmedshah <cyberahmedshah@gmail.com>
2026-03-03 23:51:49 +0500

    Added a small game

---
+++ guess the number.py
@@ -0,0 +1,13 @@
+import random
+secret_number=random.randint(1, 101)
+for i in range(1, 11):
+ guess=int(input(f"Attempt {i}, Guess the number(1-100): "))
+ if guess==secret_number:
+  print("Right! you won")
+  break
+ elif guess<secret_number:
+  print ("Too low")
+ else:
+   print("Too high")
+else:
+ print("Game over, The secret number was:", secret_number)
