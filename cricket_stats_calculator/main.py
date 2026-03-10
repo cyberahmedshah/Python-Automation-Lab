3e5c6e83eeaeac480cc5bf34e5a25516ae6d5ccf
ahmedshah <cyberahmedshah@gmail.com>
2026-03-10 22:48:08 +0500

    Added Bowling stats

--- cricket_stats_calculator/main.py
+++ cricket_stats_calculator/main.py
@@ -2,12 +2,18 @@
 from Batting import Batting_Average
 from Batting import Batting_Strike_rate
 
+# importing the functions from the Bowling module
+from Bowling import Bowling_Average
+from Bowling import Bowling_Strike_Rate
+
 #A menu for the user to choose the type of stats they want calculate
 print("Choose the type of stats you want to calculate:")
 print("1. Batting Average")
 print("2. Batting Strike Rate")
+print("3. Bowling Average")
+print("4. Bowling Strike Rate")
 
-choice = int(input("Enter your choice (1 or 2): "))
+choice = int(input("Enter your choice (1 or 2 or 3): "))
 
 if choice == 1:
     total_runs=int(input("Enter total runs scored: "))
@@ -18,3 +24,12 @@ elif choice == 2:
  total_runs=int(input("Enter total runs scored: "))
  total_balls=int(input("Enter total balls faced: "))
  print(f"The batting strike rate of batsman is: {Batting_Strike_rate(total_runs, total_balls):.2f}")
+elif choice == 3:
+    total_runs_conceded=int(input("Enter total runs conceded: "))
+    total_wickets_taken=int(input("Enter total wickets taken: "))
+    print(f"The bowling average of bowler is: {Bowling_Average(total_runs_conceded, total_wickets_taken):.2f}")
+elif choice == 4:
+    total_balls_bowled=int(input("Enter total balls bowled: "))
+    total_wickets_taken=int(input("Enter total wickets taken: "))
+    print(f"The bowling strike rate of bowler is: {Bowling_Strike_Rate(total_balls_bowled, total_wickets_taken):.2f}")
+else:    print("Invalid choice. Please choose 1, 2, 3 or 4.")
