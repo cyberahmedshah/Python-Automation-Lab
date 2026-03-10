c453308bfb91a2d242f1e719950ae73c5431ec2d
ahmedshah <cyberahmedshah@gmail.com>
2026-03-10 22:52:32 +0500

    Added bowling stats

--- cricket_stats_calculator/Bowling.py
+++ cricket_stats_calculator/Bowling.py
@@ -1,13 +1,13 @@
 def Bowling_Average(total_runs_conceded, total_wickets_taken):
     if total_wickets_taken == 0:
-        # Infinite average if no wickets taken
+        # No average if no wickets taken
         return float('0')
     else:  
      return total_runs_conceded / total_wickets_taken
     
 def Bowling_Strike_Rate(total_balls_bowled, total_wickets_taken):
     if total_wickets_taken == 0:
-        # NO strike rate if no wickets taken
+        # No strike rate if no wickets taken
         return float('0')
     else:
      return total_balls_bowled / total_wickets_taken
