c77bb32da5ec3c74d0b90b1e26c839f298ae7904
ahmedshah <cyberahmedshah@gmail.com>
2026-03-10 21:02:00 +0500

    Added a menu for user

--- cricket_stats_calculator/main.py
+++ cricket_stats_calculator/main.py
@@ -1,8 +1,20 @@
+#importing the functions from the Batting module
 from Batting import Batting_Average
 from Batting import Batting_Strike_rate
-total_runs=int(input("Enter total runs scored: "))
-total_innings=int(input("Enter total innings played: "))
-not_outs=int(input(f"Enter total not outs: "))
-total_balls=int(input("Enter total balls faced: "))
-print(f"The batting average of batsman is: {Batting_Average(total_runs, total_innings, not_outs)}")
-print(f"The batting strike rate of batsman is: {Batting_Strike_rate(total_runs, total_balls)}")
+
+#A menu for the user to choose the type of stats they want calculate
+print("Choose the type of stats you want to calculate:")
+print("1. Batting Average")
+print("2. Batting Strike Rate")
+
+choice = int(input("Enter your choice (1 or 2): "))
+
+if choice == 1:
+    total_runs=int(input("Enter total runs scored: "))
+    total_innings=int(input("Enter total innings played: "))
+    not_outs=int(input(f"Enter total not outs: "))
+    print(f"The batting average of batsman is: {Batting_Average(total_runs, total_innings, not_outs)}")
+elif choice == 2:
+ total_runs=int(input("Enter total runs scored: "))
+ total_balls=int(input("Enter total balls faced: "))
+ print(f"The batting strike rate of batsman is: {Batting_Strike_rate(total_runs, total_balls)}")
