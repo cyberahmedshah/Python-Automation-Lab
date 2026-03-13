ff82367c4c67bd4333e6ed744d548bdbe6f54532
ahmedshah <cyberahmedshah@gmail.com>
2026-03-13 16:49:57 +0500

    added close when guess is close to accurate

--- guess the number.py
+++ guess the number.py
@@ -2,9 +2,12 @@ import random
 secret_number=random.randint(1, 101)
 for i in range(1, 6):
  guess=int(input(f"Attempt {i}, Guess the number(1-100): "))
+ diff=abs(guess-secret_number)
  if guess==secret_number:
   print("Right! you won")
   break
+ elif diff<=10:
+  print("close")
  elif guess<secret_number:
   print ("Too low")
  else:
