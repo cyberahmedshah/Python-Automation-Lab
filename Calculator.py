1e283173a36aacc221ab72c0e2c1bda41b8e6049
ahmedshah <cyberahmedshah@gmail.com>
2026-03-13 06:41:48 +0500

    now user can use result for calculations

--- Calculator.py
+++ Calculator.py
@@ -1,3 +1,4 @@
+result = 0
 while True:
     operation = input("Enter operation (+, -, *, /) or 'q' to quit: ")
 
@@ -5,12 +6,20 @@ while True:
         print("Calculator closed.")
         break
 
-    if operation not in ["+", "-", "*", "/"]:
+    if operation not in ["+", "-", "*", "/", "q"]:
         print("Invalid operation. Try again.")
         continue
 
-    num1 = float(input("Enter first number: "))
-    num2 = float(input("Enter second number: "))
+    num1 = (input("Enter first number: "))
+    if num1 == "ans":
+        num1 = result
+    else:
+        num1 = float(num1) 
+    num2 = (input("Enter second number: "))
+    if num2 == "ans":
+        num2 = result
+    else:
+        num2 = float(num2)
 
     if operation == "+":
         result = num1 + num2
