1d6ad3a34c2f7c433d6521323054aa44e80a3a8e
ahmedshah <cyberahmedshah@gmail.com>
2026-03-14 08:59:33 +0500

    User can now add bowls of a over

--- cricket_stats_calculator/NRR.py
+++ cricket_stats_calculator/NRR.py
@@ -4,3 +4,10 @@ def net_run_rate(overs_faced, runs_scored, overs_bowled, runs_conceded):
     else:
         nrr = (runs_scored / overs_faced) - (runs_conceded / overs_bowled)
         return nrr
+    
+def convert_overs(overs):
+    overs = str(overs)
+    if "." in overs:
+        o, balls = overs.split(".")
+        return int(o) + int(balls) / 6
+    return float(overs)
