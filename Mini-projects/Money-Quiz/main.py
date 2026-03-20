ec593561edf5c9030463d2d8da074dfad1f655d2
ahmedshah <cyberahmedshah@gmail.com>
2026-03-20 14:26:36 +0500

    cash negative

--- Mini-projects/Money-Quiz/main.py
+++ Mini-projects/Money-Quiz/main.py
@@ -1,5 +1,9 @@
+#A mini intro to the game
 print("Welcome to the Money Quiz!" "\n" "Answer the following questions to win cash prizes!(no room for spelling mistakes)" "\n" "\n"
-"Here we go!" "\n"), 
+"Here we go!" "\n")
+
+
+#List of questions and answers
 Questions=["who invented the first paper money?","what is the name of the first paper money?","when was the first paper money invented?" 
            ,"who invented linux?", "when was linux invented?", "who was the first presidnet of united stayes?"]
 
@@ -10,9 +14,10 @@ for i in range(len(Questions)):
     answer=input("Answer: ")
     if answer.lower()==Answers[i].lower():
         print("Correct!")
-        cash+=100
+        cash+=10
     else:
-        print("Wrong!") 
+        print("Wrong!")
+        cash-=5 
 
 print("Cash won: $" + str(cash)) 
  
