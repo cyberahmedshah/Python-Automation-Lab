a65c698380300f916e26bf2c072de8b5c9345ed9
ahmedshah <cyberahmedshah@gmail.com>
2026-03-24 17:04:16 +0500

    dict practice

--- p.py
+++ p.py
@@ -10,9 +10,39 @@
 # print(feb_no(n))
 
 
-s={5,10,15}
-e={10, 20, 30}
-print(s.union(e))
-print(e.union(s))
-print(s.intersection(e))
-print(type(s))
+# s={5,10,15}
+# e={10, 20, 30}
+# # print(s.union(e))
+# # print(e.union(s))
+# # print(s.intersection(e))
+# # print(type(s))
+
+# print(s.issuperset(e))
+# print(s.isdisjoint(e))
+
+d={
+    111: "Ahmed",
+    112: "Hamza",
+    113: "Ali",
+    114: "Hassan"
+}
+
+
+d.pop(114)
+print(d)
+
+
+#print(d[int(input("Enter the id: "))])
+
+# print(d.keys())
+# print(d.values())
+
+
+# for key in d.keys():
+#     print(d[key])
+
+
+# dic={
+#     int(input("Enter the id: ")): input("Enter the name: "),
+
+# }
