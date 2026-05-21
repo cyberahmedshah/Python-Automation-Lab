0e7891a03c7c641cb3a2f0ed472c865c038e4907
ahmedshah <cyberahmedshah@gmail.com>
2026-05-21 20:26:46 +0500

    Added prize

--- Mini-projects/Money-QuizPRO/main.py
+++ Mini-projects/Money-QuizPRO/main.py
@@ -1,12 +1,13 @@
-Questions=["1.When was linux invented?", "a.1992", "b.1984", "c.2001", "d.1991", "1991"]
-print(Questions[0])
-print(Questions[1])
-print(Questions[2])
-print(Questions[3])
-print(Questions[4])
-Answers=["They are answers", "1991"]
-Answer=str(input("Enter the answer: "))
-if Answer==Answers[1]:
- print("Correct")
-else:
- print("Wrong")
+Questions=[["q"], ["1.When was linux invented?", "a.1992", "b.1984", "c.2001", "d.1991"], ["2.In which language was linux written?", "a.C", "b.Python", "c.Java", "d.C++"], ["3.Who invented linux?", "a.Bill Gates", "b.Steve Jobs", "c.Linus Torvalds", "d.Mark Zuckerberg"]]
+Answers=["They are answers", "d", "a", "c"]
+Cash=0
+for i in range(1, len(Questions)):
+ print(Questions[i])
+ Answer=str(input("Enter the right option: ")).lower()
+ if Answer==Answers[i]:
+  Cash+=5
+  print("Correct(+5$)")
+ else:
+  Cash-=5
+  print("Wrong(-5$)")
+print(f"Cash won: $ {(Cash)}") 
