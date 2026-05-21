20cc320cafae845659569caa1886893e6638ad78
ahmedshah <cyberahmedshah@gmail.com>
2026-05-21 19:41:17 +0500

    Basic structure of quiz

--- Mini-projects/Money-Quiz/main.py
+++ Mini-projects/Money-Quiz/main.py
@@ -5,7 +5,7 @@ print("Welcome to the Money Quiz!" "\n" "Answer the following questions to win c
 #List of questions and answers
 Questions=["who invented the first paper money?","what is the name of the first paper money?","when was the first paper money invented?" 
            ,"who invented linux?", "when was linux invented?", "who was the first presidnet of united stayes?"]
-
+print(type(Questions))
 Answers=["the chinese","jiaozi","7th century","linus torvalds","1991","george washington"]
 cash=0
 for i in range(len(Questions)):
---
+++ Mini-projects/Money-QuizPRO/main.py
@@ -0,0 +1,12 @@
+Questions=["1.When was linux invented?", "a.1992", "b.1984", "c.2001", "d.1991", "1991"]
+print(Questions[0])
+print(Questions[1])
+print(Questions[2])
+print(Questions[3])
+print(Questions[4])
+Answers=["They are answers", "1991"]
+Answer=str(input("Enter the answer: "))
+if Answer==Answers[1]:
+ print("Correct")
+else:
+ print("Wrong")
