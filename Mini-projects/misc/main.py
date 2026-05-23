a5e1c16bda2549b2c1dff503c03661ca87eb8c99
ahmedshah <cyberahmedshah@gmail.com>
2026-05-24 01:22:39 +0500

    Deleted a useless project

--- Mini-projects/misc/main.py
+++
@@ -1,5 +0,0 @@
-print("Hello, Automation World")
-print("This is my Python", "Automation", "Lab", sep="-")
-dict1 = {"Name": "Ahmed", "Age": 19, "Driving Eligiblity": True}
-for key, value in dict1.items():
-    print(key, ":", value)
