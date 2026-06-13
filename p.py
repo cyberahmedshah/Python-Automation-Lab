1a0dc2f45638556fdae705d86b64b3f2cd1a5044
ahmedshah <cyberahmedshah@gmail.com>
2026-06-13 06:41:11 +0500

    mind practicing

--- p.py
+++ p.py
@@ -1,17 +1,55 @@
-questions=["Who was linus torvalds?", "What is linux?", "What is git?"]
-options= ["a.scientist b.playboy c.me d.developer", "a.prod b.os c.me, d.hardware", "a.os b.prod c.me d.version control"]
-answers=["d", "b", "d"]
+que=[
 
-for i in range(len(questions)):
-    print(questions[i])
-    print(options[i])
-    a=input("Enter the option: ").lower()
-    if a==answers[i]:
-     print("correct")
+    {"question": "Who is ahmed",
+     
+     "options":[
+         "a.engineer",
+         "b.devloper",
+         "c.influencer",
+         "d.batman"
+    ],
+
+    "answer":"d"
+    },
+    
+    {"question": "what is python",
+     
+     "options":[
+         "a.human lang",
+         "b.computer lang",
+         "c.game",
+         "d.machine"
+     ],
+
+     "answer":"b"
+
+    },
+
+    {
+        "question":"what is linux",
+
+        "options":[
+            "a.machine",
+            "b.game",
+            "c.frame",
+            "d.os",
+         ],
+
+         "answer":"d"
+    }
+]
+
+for i, q in enumerate(que, start=1):
+    print(i, q["question"])
+    for o in q["options"]:
+        print(o)
+
+    answers=input("Enter the correct option: ")
+
+    if answers==q("answer"):
+            print ("correct")
     else:
-       print("Oops! You are out")
-       break
-
-
+        print("you are out")
+        break
 
 
