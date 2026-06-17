912c9f970ec1177d729d5cf4cb739641fe40ef60
ahmedshah <cyberahmedshah@gmail.com>
2026-06-17 17:06:39 +0500

    map, reduce

--- t.py
+++ t.py
@@ -1,4 +1,20 @@
-me=lambda i : print("Hello "+i)
-i=(input("Enter the number: "))
-print(me(i))
-
+students = [
+    {"name": "Ali", "score": 45},
+    {"name": "Sara", "score": 78},
+    {"name": "Ahmed", "score": 32},
+    {"name": "Zara", "score": 91},
+    {"name": "Bilal", "score": 60},
+]
+def grades(x):
+    if x['score']>=80:
+        return "A"
+    elif x['score']>=60:
+        return "B"
+    elif x['score']>=50:
+        return "C"
+    else:
+        return "Fail"
+    
+    
+grade=list(map(grades, students))
+print(grade)
