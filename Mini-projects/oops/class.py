dd7fb3146001f820132819f076121ca9bfc69f15
ahmedshah <cyberahmedshah@gmail.com>
2026-06-20 18:41:55 +0500

    basic structure oop

---
+++ Mini-projects/oops/class.py
@@ -0,0 +1,19 @@
+class student:
+     name="Ahmed"
+     rollno="43"
+     marks=91
+     def info(self):
+      print(f"{self.name} is rollno.{self.rollno} and got {self.marks} marks")
+
+s1=student()
+s1.name="Musa"
+s1.rollno="23"
+s1.marks=31
+s1.info()
+
+s2=student()
+s2.name="Haleema"
+s2.rollno="02"
+s2.marks=65
+s2.info()
+  
