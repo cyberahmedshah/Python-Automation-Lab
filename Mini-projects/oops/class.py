1211658b740236e31ea11aec9eea323258ee0c77
ahmedshah <cyberahmedshah@gmail.com>
2026-06-21 15:22:43 +0500

    basic result fetcher

--- Mini-projects/oops/class.py
+++ Mini-projects/oops/class.py
@@ -3,17 +3,30 @@ class student:
      rollno="43"
      marks=91
      def info(self):
-      print(f"{self.name} is rollno.{self.rollno} and got {self.marks} marks")
+      print(f"{self.name} is Roll no:{self.rollno} and got {self.marks} marks")
 
 s1=student()
-s1.name="Musa"
-s1.rollno="23"
-s1.marks=31
-s1.info()
 
 s2=student()
-s2.name="Haleema"
-s2.rollno="02"
-s2.marks=65
-s2.info()
-  
+s2.name="Musa"
+s2.rollno="23"
+s2.marks=31
+
+s3=student()
+s3.name="Haleema"
+s3.rollno="02"
+s3.marks=65
+
+
+result=input("Enter the rollno: ")
+
+if result=="43":
+   s1.info()
+elif result=="23":
+   s2.info()
+elif result=="02":
+   s3.info()
+else:
+   print("Roll no not found")
+     
+    
