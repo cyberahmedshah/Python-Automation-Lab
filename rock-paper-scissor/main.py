30f199e176768bce03381d06b5f4c45990ed63b9
ahmedshah <cyberahmedshah@gmail.com>
2026-06-28 01:06:02 +0500

    version 1.1

--- rock-paper-scissor/main.py
+++ rock-paper-scissor/main.py
@@ -8,19 +8,23 @@ pose=[
 computer=random.choice(pose)
 user=input("Enter any pose(rock, paper, scissor ): ").lower()
 
-if user==computer:
-    print("It's a Draw")
-elif user=="rock" and computer=="paper":
-    print("Ohh! You Lost")
-elif user=="rock" and computer=="scissor":
-    print("You Won!")
-elif user=="paper" and computer=="rock":
-    print("You Won!")
-elif user=="paper" and computer=="scissor":
-    print("Ohh! You Lost")
-elif user=="scissor" and computer=="rock":
-    print("Ohh! You Lost")
-elif user=="scissor" and computer=="paper":
-    print("You Won!")
-else:
-    print("Enter valid pose")
+while True:
+ if user==computer:
+     print("It's a Draw")
+ elif user=="rock" and computer=="paper":
+     print("Ohh! You Lost")
+ elif user=="rock" and computer=="scissor":
+     print("You Won!")
+ elif user=="paper" and computer=="rock":
+     print("You Won!")
+ elif user=="paper" and computer=="scissor":
+     print("Ohh! You Lost")
+ elif user=="scissor" and computer=="rock":
+     print("Ohh! You Lost")
+ elif user=="scissor" and computer=="paper":
+     print("You Won!")
+ elif user==("q"):
+     print("Game exited")
+     break
+ else:
+     print("Enter valid pose")
