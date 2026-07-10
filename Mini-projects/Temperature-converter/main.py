e0058d6bc491da0d91d82617b68c18a7b53d28ae
ahmedshah <cyberahmedshah@gmail.com>
2026-07-10 10:14:48 +0500

    used decorater to greet user

--- Mini-projects/Temperature-converter/main.py
+++ Mini-projects/Temperature-converter/main.py
@@ -1,8 +1,19 @@
+def greet(fx):
+  def wrapper():
+   print("Hello Sir")
+   print(f"The temperature in degree is {fx():.2f}") 
+   print("Thanks for using our tool")
+  return wrapper
+
+
+@greet
 def celsuis():
-  a=int(input("Enter the temperature in fahrenheit: "))
+  a=float(input("Enter the temperature in fahrenheit: "))
   b=a-32
   return b/1.8
 
-print(celsuis())
+celsuis()
+
+
 
 
